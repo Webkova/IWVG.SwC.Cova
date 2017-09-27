9bc7d72653e6e07baf2a55bec82ad0b85dc0f6b0
egraciani <egraciani@172.16.40.70>
2017-09-27 21:34:15 +0200

    Including changes on class Point.

--- src/main/java/es/upm/miw/forge/ecp2/Point.java
+++ src/main/java/es/upm/miw/forge/ecp2/Point.java
@@ -1,11 +1,12 @@
 package es.upm.miw.forge.ecp2;
 
 public class Point {
-    private int x, y;
+    private int x, y, z;
 
-    public Point(int x, int y) {
+    public Point(int x, int y, int z) {
         this.x = x;
         this.y = y;
+        this.z = z;
     }
 
     public Point(int xy) {
@@ -13,7 +14,7 @@ public class Point {
     }
 
     public Point() {
-        this(0, 0);
+        this(0, 0, 0);
     }
 
     public int getX() {
@@ -23,9 +24,13 @@ public class Point {
     public int getY() {
         return this.y;
     }
+    
+    public int getZ() {
+        return this.z;
+    }
 
     public double module() {
-        return Math.sqrt(this.x * this.x + this.y * this.y);
+        return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
     }
 
     public double phase() {
@@ -35,11 +40,12 @@ public class Point {
     public void translateOrigin(Point origin) {
         this.x -= origin.getX();
         this.y -= origin.getY();
+        this.z -= origin.getZ();
     }
 
     @Override
     public String toString() {
-        return "Point[" + x + "," + y + "]";
+        return "Point[" + x + "," + y + "," + z + ]";
     }
     
 }
